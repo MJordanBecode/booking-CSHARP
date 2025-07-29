e60024e273d1da558e8e4891aa4ea75eb82d6931
shishipc <chapon.antoine.pro@gmail.com>
2025-07-29 15:27:14 +0200

    erreur using reglÃer

--- Booking/Service/OfferService.cs
+++ Booking/Service/OfferService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Booking.Data;
 using Solution.Models;
 using Solution.Service;
