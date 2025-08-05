084b7d0dbeaca506564aa4c5a874e6f7f0ac44f8
MasyJordan <masyjordan@gmail.com>
2025-08-05 16:58:24 +0200

    Logout

--- Booking/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Booking/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -40,7 +40,7 @@ namespace Booking.Areas.Identity.Pages.Account
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToPage("/");
             }
         }
     }
